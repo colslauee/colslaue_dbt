b7aee6c177b5d7f84c7beb1dba2893d6c771d8c8
colslauee <colslaue@gmail.com>
2023-08-29 21:27:48 -0300

    created datasets `report_tables` and `source_data`
    - created datasets `report_tables` and `source_data`
    - updated dbt_project.yml to set default schema for report_tables directory to `report_tables` and default schema for source_data directory to `source_data`
    - updated dbt_project.yml to set default to "materialized: table" for both the report_tables directory and the source_data directory
    - created generate_schema_name.sql macro to prevent dbt from prefixing each schema with `dbt_data`

---
+++ models/report_tables/report__full_orders.sql
@@ -0,0 +1,43 @@
+WITH order_totals AS(
+	SELECT
+		order_id
+		,SUM(sale_price) AS total_order_value
+
+	FROM {{ source('thelook_ecommerce', 'order_items') }}
+	GROUP BY 1
+)
+
+SELECT
+  orders.order_id AS order_id
+  ,users.id AS user_id
+  ,users.first_name AS user_first_name
+  ,users.last_name AS user_last_name
+  ,CONCAT(users.first_name, ' ', users.last_name) AS user_full_name
+  ,users.email AS user_email
+  ,users.age AS user_age
+  ,users.gender AS user_gender
+  ,users.state AS user_state
+  ,users.street_address AS user_street_address
+  ,users.postal_code AS user_postal_code
+  ,users.city AS user_city
+  ,users.country AS user_country
+  ,users.latitude AS user_latitude
+  ,users.longitude AS user_longitude
+  ,users.traffic_source AS user_traffic_source
+  ,users.created_at AS user_created_at
+  ,orders.status AS order_status
+  ,orders.gender AS order_gender
+  ,orders.created_at AS order_created_at
+  ,orders.returned_at AS order_returned_at
+  ,orders.shipped_at AS order_shipped_at
+  ,orders.delivered_at AS order_delivered_at
+  ,orders.num_of_item AS order_num_of_item
+  ,ROUND(order_totals.total_order_value, 2) AS total_order_value
+
+FROM {{ source('thelook_ecommerce', 'orders') }} AS orders
+
+LEFT JOIN {{ source('thelook_ecommerce', 'users') }} AS users
+ON orders.user_id = users.id
+
+LEFT JOIN order_totals
+USING (order_id)
